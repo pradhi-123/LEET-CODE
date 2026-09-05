d61c4f6a53e5415d2f796ce1ed6568a8ca815588
Pradhiksha Anandkumar <76558544+pradhi-123@users.noreply.github.com>
2026-09-05 19:50:56 +0530

    Time: 0 ms (100.00%) | Memory: 62.6 MB (74.53%) - LeetSync

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.c
@@ -0,0 +1,18 @@
+struct ListNode* modifiedList(int* nums, int numsSize, struct ListNode* head) {
+    int arr[100001] = {0};
+    for(int i = 0; i < numsSize; i++)
+        arr[nums[i]] = 1;
+
+    while(head != NULL && arr[head->val])
+        head = head->next;
+
+    struct ListNode* temp = head;
+
+    while(temp != NULL && temp->next != NULL) {
+        if(arr[temp->next->val])
+            temp->next = temp->next->next;
+        else
+            temp = temp->next;
+    }
+    return head;
+}
