fa4ddbb67b2e409c8c6b2bf61e4d91c0fc5c12b2
Pradhiksha Anandkumar <76558544+pradhi-123@users.noreply.github.com>
2026-09-09 23:18:22 +0530

    Time: 2 ms (99.75%) | Memory: 43.8 MB (17.44%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,30 +1,38 @@
 class Solution {
-    public String multiply(String num1, String num2) 
-    {
+    public String multiply(String num1, String num2) {
+
         if (num1.equals("0") || num2.equals("0"))
             return "0";
+
         int n = num1.length();
         int m = num2.length();
-        int[] result = new int[n + m];
+
+        int[] res = new int[n + m];
+
         for (int i = n - 1; i >= 0; i--) {
-            for (int j = m - 1; j >= 0; j--)
-            {
-                int a = num1.charAt(i) - '0';
+            int a = num1.charAt(i) - '0';
+
+            for (int j = m - 1; j >= 0; j--) {
                 int b = num2.charAt(j) - '0';
-                int product = a * b;
-                int p1 = i + j;
-                int p2 = i + j + 1;
-                int sum = product + result[p2];
-                result[p2] = sum % 10;
-                result[p1] += sum / 10;
+
+                int pos = i + j + 1;
+
+                int value = a * b + res[pos];
+
+                res[pos] = value % 10;
+                res[pos - 1] += value / 10;
             }
         }
-        StringBuilder ans = new StringBuilder();
-        for (int x : result) {
-            if (ans.length() == 0 && x == 0)
-                continue;
-            ans.append(x);
-        }
-        return ans.toString();
+
+        StringBuilder sb = new StringBuilder(n + m);
+
+        int i = 0;
+        while (i < res.length && res[i] == 0)
+            i++;
+
+        while (i < res.length)
+            sb.append(res[i++]);
+
+        return sb.toString();
     }
 }
